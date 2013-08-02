3ab1a1a267140457092902cce1acfe5b96dc584e
fnord0 <fnord0@riseup.net>
2013-08-02 08:18:16 -0700

    testing, testing

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,24 @@ from __future__ import unicode_literals
 
 AUTHOR = u'fnord0'
 SITENAME = u'Dweller on the Threshold.net'
-SITEURL = ''
+SITEURL = 'https://fnord0.github.io/pelican'
+#SITEURL = 'posts/slug/index.html'
+#SAVE_AS = 'posts/slug/index.html'
+ARTICLE_URL = 'posts/{slug}'
+ARTICLE_SAVE_AS = 'posts/{slug}.html'
+PAGE_URL = '{slug}' 
+PAGE_SAVE_AS = '{slug}.html'
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}.html'
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}.html'
+TYPOGRIFY = True
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'feeds/atom.xml'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
 
@@ -29,3 +44,6 @@ DEFAULT_PAGINATION = 30
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+#FILES_TO_COPY = ( ('extra/robots.txt', 'robots.txt'),
+#                  ('extra/.htaccess', '.htaccess') )
